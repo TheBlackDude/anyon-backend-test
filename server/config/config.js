e91d4176f8691da17dd7656912a9b8407cdd272b
Osman Jalloh <mudu.jalloh@gn.ehealthafrica.org>
2018-01-18 14:21:22 +0000

    feat: add db config.

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+/* Database URL */
+module.exports = {
+  /*
+   * don't put your credentials here on a production app
+   * i just put it here for testing
+   */
+  url: mongodb://anyon:12345@ds261527.mlab.com:61527/anyon
+}
